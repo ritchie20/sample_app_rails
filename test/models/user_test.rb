ec43d4aa7e5a1a7d95208440a2580957908d47fe
ritchie20 <oscar.rubio.ma@gmail.com>
2015-08-27 19:12:09 +0000

    lab capitulo 6 terminado

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,6 +52,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
   
+  test "email addresses should be saved in lower-case" do
+    mixed_case_email = "OsCar@RubiO.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
